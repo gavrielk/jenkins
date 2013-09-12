8d9726e6b6695b0857814632bb2b0fe9648cf2ac
gavrielk <gkarasin@gmail.com>
2013-09-12 15:09:19 -0700

    successfully added link to sidebar. Next need to create a new Jenkins page under plugins/testPlugin

--- jenkins/test/src/main/java/org/jenkinsci/plugins/test/Messages.java
+++ jenkins/test/src/main/java/org/jenkinsci/plugins/test/Messages.java
@@ -2,79 +2,21 @@
 
 package org.jenkinsci.plugins.test;
 
-import org.jvnet.localizer.Localizable;
-import org.jvnet.localizer.ResourceBundleHolder;
-
-@SuppressWarnings({
-    "",
-    "PMD"
-})
 public class Messages {
+    
+    private static String DisplayText = "Test Page";
+    private static String Url = "plugin/testPlugin";
+    private static String Description = "This is my test plugin";
 
-    private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
-
-    /**
-     * Disk usage
-     * 
-     */
     public static String DisplayName() {
-        return holder.format("DisplayName");
+        return DisplayText;
     }
 
-    /**
-     * Disk usage
-     * 
-     */
-    public static Localizable _DisplayName() {
-        return new Localizable(holder, "DisplayName");
-    }
-
-    /**
-     * diskUsage
-     * 
-     */
     public static String UrlName() {
-        return holder.format("UrlName");
+        return Url;
     }
 
-    /**
-     * diskUsage
-     * 
-     */
-    public static Localizable _UrlName() {
-        return new Localizable(holder, "UrlName");
-    }
-
-    /**
-     * disk usage
-     * 
-     */
-    public static String ProjectDiskUsage() {
-        return holder.format("ProjectDiskUsage");
-    }
-
-    /**
-     * disk usage
-     * 
-     */
-    public static Localizable _ProjectDiskUsage() {
-        return new Localizable(holder, "ProjectDiskUsage");
-    }
-
-    /**
-     * Displays per-project disk usage
-     * 
-     */
     public static String Description() {
-        return holder.format("Description");
+        return Description;
     }
-
-    /**
-     * Displays per-project disk usage
-     * 
-     */
-    public static Localizable _Description() {
-        return new Localizable(holder, "Description");
-    }
-
 }
