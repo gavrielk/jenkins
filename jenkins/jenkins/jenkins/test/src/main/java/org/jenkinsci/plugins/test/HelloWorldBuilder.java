29a1c76a7c8de73bdb49c38a16833575d8192f1a
gavrielk <gkarasin@gmail.com>
2013-09-12 14:30:13 -0700

    adding index.jelly file to create link on sidepanel

---
+++ jenkins/jenkins/jenkins/test/src/main/java/org/jenkinsci/plugins/test/HelloWorldBuilder.java
@@ -0,0 +1,31 @@
+package org.jenkinsci.plugins.test;
+
+import hudson.Launcher;
+import hudson.Plugin;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.tasks.Builder;
+
+/**
+ * Sample {@link Builder}.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link HelloWorldBuilder} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
+ * method will be invoked. 
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class HelloWorldBuilder extends Plugin {
+
+    
+   
+}
+
