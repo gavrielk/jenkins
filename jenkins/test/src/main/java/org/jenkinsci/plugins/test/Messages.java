5b619591ae33857e9e79037091f737d1f4907011
gavrielk <gkarasin@gmail.com>
2013-09-22 03:03:23 -0700

    (small update)successfully added link to sidebar. Next need to create a new Jenkins page under plugins/testPlugin

--- jenkins/test/src/main/java/org/jenkinsci/plugins/test/Messages.java
+++ jenkins/test/src/main/java/org/jenkinsci/plugins/test/Messages.java
@@ -5,7 +5,7 @@ package org.jenkinsci.plugins.test;
 public class Messages {
     
     private static String DisplayText = "Test Page";
-    private static String Url = "plugin/testPlugin";
+    private static String Url = "plugin/test";
     private static String Description = "This is my test plugin";
 
     public static String DisplayName() {
